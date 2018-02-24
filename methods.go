38ae7e721be891efb4a4638e4be190b10f302716
Jozef Reisinger <jozef.reisinger@telekom.com>
2018-02-24 16:32:05 +0100

    methods

---
+++ methods.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Person struct {
+	Name string
+}
+
+type Android struct {
+	Person // embedded type (or anonymous field)
+	Model  string
+}
+
+func (p *Person) talk() {
+	fmt.Println("Hi, my name is", p.Name)
+}
+
+func main() {
+	p := Person{Name: "John"}
+
+	a := new(Android)
+	a.Name = "R2D2"
+
+	p.talk()
+	a.talk()
+}
