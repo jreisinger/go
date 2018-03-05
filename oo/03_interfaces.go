d46d9f2d0286004923971ae30a3ea481e0a3f7cf
Jozef Reisinger <jozef.reisinger@telekom.com>
2018-03-05 08:50:30 +0100

    exercise 3

--- oo/03_interfaces.go
+++ oo/03_interfaces.go
@@ -21,6 +21,7 @@ type Rectangle struct {
 // order to implement the interface.
 type Shape interface {
 	area() float64
+	perimeter() float64
 }
 
 func distance(x1, y1, x2, y2 float64) float64 {
@@ -33,10 +34,21 @@ func (c *Circle) area() float64 {
 	return math.Pi * c.r * c.r
 }
 
+func (c *Circle) perimeter() float64 {
+	return 2 * math.Pi * c.r
+}
+
 func (r *Rectangle) area() float64 {
 	l := distance(r.x1, r.y1, r.x1, r.y2)
 	w := distance(r.x1, r.y1, r.x2, r.y1)
 	return l * w
+}
+
+func (r *Rectangle) perimeter() float64 {
+	l := distance(r.x1, r.y1, r.x1, r.y2)
+	w := distance(r.x1, r.y1, r.x2, r.y1)
+	//return 2 * l + 2 * w
+	return 2 * (l + w)
 }
 
 // Interface type used here as an argument to a function.
