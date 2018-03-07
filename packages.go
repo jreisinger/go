df12f9dc5e920dc99aa61632decc0cdb548141d3
Jozef Reisinger <jozef.reisinger@telekom.com>
2018-03-07 08:47:56 +0100

    Walk a directory recursively

--- packages.go
+++ packages.go
@@ -5,7 +5,14 @@ package main
 // Many of the libraries (packages) included with Go require specialized,
 // domain specific knowledge (e.g. cryptography).
 
-import ( "fmt" ; "strings" ; "os" ; "io/ioutil" ; "log" )
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+    "path/filepath"
+)
 
 //
 // Strings
@@ -13,57 +20,57 @@ import ( "fmt" ; "strings" ; "os" ; "io/ioutil" ; "log" )
 
 // strings functions
 func mystrings() {
-    // func Contains(s, substr string) bool
-    fmt.Println(strings.Contains("test", "es"))
-    // => true
+	// func Contains(s, substr string) bool
+	fmt.Println(strings.Contains("test", "es"))
+	// => true
 
-    // func Count(s, sep string) int
-    fmt.Println(strings.Count("test", "t"))
-    // => 2
+	// func Count(s, sep string) int
+	fmt.Println(strings.Count("test", "t"))
+	// => 2
 
-    // func HasPrefix(s, prefix string) bool
-    fmt.Println(strings.HasPrefix("test", "te"))
-    // => true
+	// func HasPrefix(s, prefix string) bool
+	fmt.Println(strings.HasPrefix("test", "te"))
+	// => true
 
-    // func HasSuffix(s, suffix string) bool
-    fmt.Println(strings.HasSuffix("test", "st"))
-    // => true
+	// func HasSuffix(s, suffix string) bool
+	fmt.Println(strings.HasSuffix("test", "st"))
+	// => true
 
-    // func Index(s, substr string) int
-    // -1 if not found
-    fmt.Println(strings.Index("test", "e"))
-    // => 1
+	// func Index(s, substr string) int
+	// -1 if not found
+	fmt.Println(strings.Index("test", "e"))
+	// => 1
 
-    // func Join(a []string, sep string) string
-    fmt.Println(strings.Join([]string{"a", "b"}, "-"))
-    // => "a-b"
+	// func Join(a []string, sep string) string
+	fmt.Println(strings.Join([]string{"a", "b"}, "-"))
+	// => "a-b"
 
-    // func Repeat(s string, count int) string
-    fmt.Println(strings.Repeat("a", 5))
-    // => "aaaaa"
+	// func Repeat(s string, count int) string
+	fmt.Println(strings.Repeat("a", 5))
+	// => "aaaaa"
 
-    // func Replace(s, old, new string, n int) string
-    fmt.Println(strings.Replace("aaaa", "a", "b", 2))
-    // => "bbaa"
+	// func Replace(s, old, new string, n int) string
+	fmt.Println(strings.Replace("aaaa", "a", "b", 2))
+	// => "bbaa"
 
-    // func Split(s, sep string) []string
-    fmt.Println(strings.Split("a-b-c", "-"))
-    // => []string{"a", "b", "c"}
+	// func Split(s, sep string) []string
+	fmt.Println(strings.Split("a-b-c", "-"))
+	// => []string{"a", "b", "c"}
 
-    // func ToLower(s string) string
-    fmt.Println(strings.ToLower("TEST"))
-    // => "test"
+	// func ToLower(s string) string
+	fmt.Println(strings.ToLower("TEST"))
+	// => "test"
 
-    // func ToUpper(s string) string
-    fmt.Println(strings.ToUpper("test"))
-    // => "TEST"
+	// func ToUpper(s string) string
+	fmt.Println(strings.ToUpper("test"))
+	// => "TEST"
 
-    // Sometimes we need to work with strings as binary data. To convert a
-    // string to a slice of bytes (and vice versa):
-    arr := []byte("test")
-    str := string([]byte{'T','E','S','T'})
-    fmt.Println(arr)
-    fmt.Println(str)
+	// Sometimes we need to work with strings as binary data. To convert a
+	// string to a slice of bytes (and vice versa):
+	arr := []byte("test")
+	str := string([]byte{'T', 'E', 'S', 'T'})
+	fmt.Println(arr)
+	fmt.Println(str)
 }
 
 //
@@ -71,63 +78,72 @@ func mystrings() {
 //
 
 // Open and print a file
-func readFile (fileName string) {
-    file, err := os.Open(fileName)
-    if err != nil {
-        // handle the error here
-        return
-    }
-    defer file.Close()
+func readFile(fileName string) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		// handle the error here
+		return
+	}
+	defer file.Close()
 
-    // get the file size
-    stat, err := file.Stat()
-    if err != nil {
-        return
-    }
+	// get the file size
+	stat, err := file.Stat()
+	if err != nil {
+		return
+	}
 
-    // read the file
-    bs := make([]byte, stat.Size())
-    _, err = file.Read(bs)
-    if err != nil {
-        return
-    }
+	// read the file
+	bs := make([]byte, stat.Size())
+	_, err = file.Read(bs)
+	if err != nil {
+		return
+	}
 
-    str := string(bs)
-    fmt.Println(str)
+	str := string(bs)
+	fmt.Println(str)
 }
 
 // Shorter way to open and print a file
-func readFile2 (fileName string) {
-    content, err := ioutil.ReadFile(fileName)
-    if err != nil {
-        log.Fatal(err)
-    }
+func readFile2(fileName string) {
+	content, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    fmt.Printf("File contents: %s", content)
+	fmt.Printf("File contents: %s", content)
 }
 
 // Get the contents of a directory
-func readDir (dirname string) {
-    dir, err := os.Open(dirname)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer dir.Close()
+func readDir(dirname string) {
+	dir, err := os.Open(dirname)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer dir.Close()
 
-    fileInfos, err := dir.Readdir(-1) // -1 => all dir entries
-    if err != nil {
-        log.Fatal(err)
-    }
-    for _, fi := range fileInfos {
-        fmt.Println(fi.Name())
-    }
+	fileInfos, err := dir.Readdir(-1) // -1 => all dir entries
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, fi := range fileInfos {
+		fmt.Println(fi.Name())
+	}
+}
+
+// Walk a directory (folder) recurively
+func walkDir(dirname string) {
+    filepath.Walk(dirname, func(path string, info os.FileInfo, err error) error {
+        fmt.Println(path)
+        return nil
+    })
 }
 
 func main() {
-    mystrings()
+	mystrings()
 
-    readFile("/etc/hosts")
-    readFile2("/etc/hosts")
+	readFile("/etc/hosts")
+	readFile2("/etc/hosts")
 
-    readDir(".")
+	readDir(".")
+    walkDir("/tmp")
 }
