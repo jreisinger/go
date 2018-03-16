6477ad4fb21f22cf8d89c45c1ea4215339572109
Jozef Reisinger <jozef.reisinger@telekom.com>
2018-03-16 08:37:14 +0100

    hello world HTTP server

---
+++ http-server.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func hello(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set(
+		"Content-Type",
+		"text/html",
+	)
+	io.WriteString(
+		res,
+		`
+        <DOCTYPE html>
+        <html>
+            <head>
+                <title>Hello, world</title>
+            </head>
+            <body>
+                Hello, world!
+            </body>
+        </html>
+        `,
+	)
+}
+
+func main() {
+    // handle "/hello" route by calling hello()
+	http.HandleFunc("/hello", hello)
+
+	http.ListenAndServe(":9000", nil)
+}
