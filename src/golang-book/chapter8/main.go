de5e5cfd9c9c0ee9b7df656e56d306bcb5974f02
Jozef Reisinger <jozef.reisinger@telekom.com>
2018-03-19 09:04:33 +0100

    package directory structure

---
+++ src/golang-book/chapter8/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+import "golang-book/chapter8/math"
+
+func main() {
+    xs := []float64{1,2,3,4}
+    avg := math.Average(xs)
+    fmt.Println(avg)
+
+    min := math.Min(xs)
+    fmt.Println(min)
+
+    max := math.Max(xs)
+    fmt.Println(max)
+}
---
+++ src/golang-book/chapter8/math/math.go
@@ -0,0 +1,30 @@
+package math
+
+func Average(xs []float64) float64 {
+    total := float64(0)
+    for _, x := range xs {
+        total += x
+    }
+    return total / float64(len(xs))
+}
+
+func Min(xs []float64) float64 {
+    var min float64
+    min, xs = xs[0], xs[1:]
+    for _, x := range xs {
+        if x < min {
+            min = x
+        }
+    }
+    return min
+}
+
+func Max(xs []float64) float64 {
+    var max float64
+    for i, x := range xs {
+        if i == 0 || x > max {
+            max = x
+        }
+    }
+    return max
+}
