cb93e901b5bd5173b14ce41f9b56d8c6ba702a1f
Jozef Reisinger <jozef.reisinger@telekom.com>
2018-03-19 09:06:48 +0100

    add comments, i.e. documentation

--- src/golang-book/chapter8/math/math.go
+++ src/golang-book/chapter8/math/math.go
@@ -8,6 +8,7 @@ func Average(xs []float64) float64 {
     return total / float64(len(xs))
 }
 
+// Find a minimun value in a slice of floats
 func Min(xs []float64) float64 {
     var min float64
     min, xs = xs[0], xs[1:]
@@ -19,6 +20,7 @@ func Min(xs []float64) float64 {
     return min
 }
 
+// Find a maximum value in a slice of floats
 func Max(xs []float64) float64 {
     var max float64
     for i, x := range xs {
