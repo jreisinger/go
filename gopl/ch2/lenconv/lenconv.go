732528771cbc9924a9f0d05eb58493c31bc9d649
Jozef Reisinger <jozef.reisinger@telekom.com>
2018-09-18 13:02:26 +0200

    update file gopl/ch2/lenconv/lenconv.go

---
+++ gopl/ch2/lenconv/lenconv.go
@@ -0,0 +1,10 @@
+// Package lenconv performs Meters and Feet conversions.
+package lenconv
+
+type Meters float64
+type Feet float64
+
+func (m Meters) String() string { return fmt.Sprintf("%g m", m) }
+func (f Feet) String() string { return fmt.Sprintf("%g ft", f) }
+
+func MToF(m Meters) Feet { return Feet(m/0.3048) }
