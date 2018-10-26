cfbd9148b4603fcbe3ff9f4f32e989dc09d7e44c
Jozef Reisinger <jozef.reisinger@telekom.com>
2018-10-26 08:58:32 +0200

    sort ByAge also

--- sort.go
+++ sort.go
@@ -35,13 +35,28 @@ func (ps ByName) Swap(i, j int) {
 	ps[i], ps[j] = ps[j], ps[i]
 }
 
+type ByAge []Person
+func (ps ByAge) Len() int {
+    return len(ps)
+}
+func (ps ByAge) Less(i, j int) bool {
+    return ps[i].Age < ps[j].Age
+}
+func (ps ByAge) Swap(i, j int) {
+    ps[i], ps[j] = ps[j], ps[i]
+}
+
 func main() {
 	kids := []Person{
 		{"Eli", 2},
 		{"Dado", 2},
 		{"Matu", 4},
+        {"Z", 1},
 	}
 
 	sort.Sort(ByName(kids))
 	fmt.Println(kids)
+
+    sort.Sort(ByAge(kids))
+    fmt.Println(kids)
 }
