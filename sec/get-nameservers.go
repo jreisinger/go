867e6dee8e8e9409271c384cf63ea9962aec2556
Jozef Reisinger <jozef.reisinger@telekom.com>
2018-11-02 12:06:23 +0100

    update file sec/get-nameservers.go

---
+++ sec/get-nameservers.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+    "os"
+    "fmt"
+    "log"
+    "net"
+)
+
+func main() {
+    if len(os.Args) != 2 {
+        log.Fatal("No domain name supplied.")
+    }
+    domain := os.Args[1]
+
+    nsRecords, err := net.LookupNS(domain)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    for _, ns := range nsRecords {
+        fmt.Println(ns.Host)
+    }
+}
