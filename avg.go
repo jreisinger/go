e9d0286f6d28e2b40eac2ca7a7e0a36531c782d5
Jozef Reisinger <jozef.reisinger@telekom.com>
2018-11-08 14:11:01 +0100

    update file avg.go

--- avg.go
+++ avg.go
@@ -3,15 +3,15 @@ package main
 import "fmt"
 
 func main() {
-    nums := []float64{1,2,3,4,5}
-    fmt.Println(avg(nums))
+	nums := []float64{1, 2, 3, 4, 5}
+	fmt.Println(avg(nums))
 }
 
 func avg(nums []float64) float64 {
-    //panic("not implemented")
-    sum := 0.0
-    for _, v := range nums {
-        sum += v
-    }
-    return sum / float64(len(nums))
+	//panic("not implemented")
+	sum := 0.0
+	for _, v := range nums {
+		sum += v
+	}
+	return sum / float64(len(nums))
 }
