4a86d8bfacf264e71b84c47ccc1d0ee19b114165
Jozef Reisinger <jozef.reisinger@telekom.com>
2018-11-17 10:45:42 +0100

    update files sec/get-nameservers.go, sec/get-netifs.go

--- sec/get-nameservers.go
+++ sec/get-nameservers.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-    "os"
-    "fmt"
-    "log"
-    "net"
+	"fmt"
+	"log"
+	"net"
+	"os"
 )
 
 func main() {
-    if len(os.Args) != 2 {
-        log.Fatal("No domain name supplied.")
-    }
-    domain := os.Args[1]
+	if len(os.Args) != 2 {
+		log.Fatal("No domain name supplied.")
+	}
+	domain := os.Args[1]
 
-    nsRecords, err := net.LookupNS(domain)
-    if err != nil {
-        log.Fatal(err)
-    }
+	nsRecords, err := net.LookupNS(domain)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    for _, ns := range nsRecords {
-        fmt.Println(ns.Host)
-    }
+	for _, ns := range nsRecords {
+		fmt.Println(ns.Host)
+	}
 }
--- sec/get-netifs.go
+++ sec/get-netifs.go
@@ -1,23 +1,23 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "github.com/google/gopacket/pcap"
+	"fmt"
+	"github.com/google/gopacket/pcap"
+	"log"
 )
 
 func main() {
-    // Find all devices
-    devices, err := pcap.FindAllDevs()
-    if err != nil {
-        log.Fatal(err)
-    }
+	// Find all devices
+	devices, err := pcap.FindAllDevs()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    // Print device information
-    for _, device := range devices {
-        fmt.Printf("%s (%s)\n", device.Name, device.Description)
-        for _, addr := range device.Addresses {
-            fmt.Printf("* %s/%d\n", addr.IP, addr.Netmask)
-        }
-    }
+	// Print device information
+	for _, device := range devices {
+		fmt.Printf("%s (%s)\n", device.Name, device.Description)
+		for _, addr := range device.Addresses {
+			fmt.Printf("* %s/%d\n", addr.IP, addr.Netmask)
+		}
+	}
 }
